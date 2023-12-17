6bf4a36b24c3f5ccb9c133e8b5c6138fb7295e1b
FleetingTimePaul <83365885@qq.com>
2023-12-17 11:41:49 +0800

    update.

--- pb/skytree_backend/skytree_backend_grpc.pb.go
+++ pb/skytree_backend/skytree_backend_grpc.pb.go
@@ -19,6 +19,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type SkyteeBackendClient interface {
 	GetCrossChainTxStatus(ctx context.Context, in *GetCrossChainTxStatusRequest, opts ...grpc.CallOption) (*GetCrossChainTxStatusResponse, error)
+	DepositPoint(ctx context.Context, in *DepositPointRequest, opts ...grpc.CallOption) (*DepositPointResponse, error)
 }
 
 type skyteeBackendClient struct {
@@ -38,11 +39,21 @@ func (c *skyteeBackendClient) GetCrossChainTxStatus(ctx context.Context, in *Get
 	return out, nil
 }
 
+func (c *skyteeBackendClient) DepositPoint(ctx context.Context, in *DepositPointRequest, opts ...grpc.CallOption) (*DepositPointResponse, error) {
+	out := new(DepositPointResponse)
+	err := c.cc.Invoke(ctx, "/skytree_backend.api.SkyteeBackend/DepositPoint", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // SkyteeBackendServer is the server API for SkyteeBackend service.
 // All implementations must embed UnimplementedSkyteeBackendServer
 // for forward compatibility
 type SkyteeBackendServer interface {
 	GetCrossChainTxStatus(context.Context, *GetCrossChainTxStatusRequest) (*GetCrossChainTxStatusResponse, error)
+	DepositPoint(context.Context, *DepositPointRequest) (*DepositPointResponse, error)
 	mustEmbedUnimplementedSkyteeBackendServer()
 }
 
@@ -52,6 +63,9 @@ type UnimplementedSkyteeBackendServer struct {
 
 func (UnimplementedSkyteeBackendServer) GetCrossChainTxStatus(context.Context, *GetCrossChainTxStatusRequest) (*GetCrossChainTxStatusResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetCrossChainTxStatus not implemented")
+}
+func (UnimplementedSkyteeBackendServer) DepositPoint(context.Context, *DepositPointRequest) (*DepositPointResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DepositPoint not implemented")
 }
 func (UnimplementedSkyteeBackendServer) mustEmbedUnimplementedSkyteeBackendServer() {}
 
@@ -84,6 +98,24 @@ func _SkyteeBackend_GetCrossChainTxStatus_Handler(srv interface{}, ctx context.C
 	return interceptor(ctx, in, info, handler)
 }
 
+func _SkyteeBackend_DepositPoint_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DepositPointRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SkyteeBackendServer).DepositPoint(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/skytree_backend.api.SkyteeBackend/DepositPoint",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SkyteeBackendServer).DepositPoint(ctx, req.(*DepositPointRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // SkyteeBackend_ServiceDesc is the grpc.ServiceDesc for SkyteeBackend service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -94,6 +126,10 @@ var SkyteeBackend_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetCrossChainTxStatus",
 			Handler:    _SkyteeBackend_GetCrossChainTxStatus_Handler,
+		},
+		{
+			MethodName: "DepositPoint",
+			Handler:    _SkyteeBackend_DepositPoint_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
